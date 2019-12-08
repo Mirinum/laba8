ff5fcf3ebd05cfad16506a320894d92fce2b6269
Mirinum <edien.pierce.wd@gmail.com>
2019-12-08 21:30:30 +0300

    Корректирование глупых ошибок
    Подготовил чёртову функцию к написанию
    Хотя бы теперь она работает

--- func1.cpp
+++ func1.cpp
@@ -2,6 +2,75 @@
 
 using namespace std;
 
+
+
+
+List::List(){
+	Size = 0;
+	head = nullptr;
+}
+
+
+List::~List(){
+	
+}
+
+
+void List::push_back(TE data){
+	if(head == nullptr){
+		head = new Node(data);
+	}
+	else{
+		Node *current = this->head;
+		while(current->pNext != nullptr){
+			current = current->pNext;
+		}
+		current->pNext = new Node(data);
+	}
+	Size++;
+}
+	
+
+Node* List::operator()(const int index){	
+	Node *current = this -> head;
+	int counter = 0;
+	while(current!=nullptr){
+		if(counter == index){
+			return current;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+Node* List::pNext(const int index){	
+	Node *current = this -> head;
+	int counter = 0;
+	while(current!=nullptr){
+		if(counter == index){
+			return current->pNext;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+TE& List::operator[](const int index){	
+	Node *current = this -> head;
+	int counter = 0;
+	while(current!=nullptr){
+		if(counter == index){
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+
+
 int merge(List L1, List L2){
+	cout << L1[2] << L1[1];
+	cout << endl<<"Lol";
 	return 0;
 }
--- func1.h
+++ func1.h
@@ -8,7 +8,6 @@ using namespace std;
 
 typedef double TE;
 
-
 struct Node{
 	public:
 		Node *pNext;
@@ -34,70 +33,6 @@ struct List{
 		
 };
 
-
-List::List(){
-	Size = 0;
-	head = nullptr;
-}
-
-
-List::~List(){
-	
-}
-
-
-void List::push_back(TE data){
-	if(head == nullptr){
-		head = new Node(data);
-	}
-	else{
-		Node *current = this->head;
-		while(current->pNext != nullptr){
-			current = current->pNext;
-		}
-		current->pNext = new Node(data);
-	}
-	Size++;
-}
-	
-
-Node* List::operator()(const int index){	
-	Node *current = this -> head;
-	int counter = 0;
-	while(current!=nullptr){
-		if(counter == index){
-			return current;
-		}
-		current = current->pNext;
-		counter++;
-	}
-}
-
-Node* List::pNext(const int index){	
-	Node *current = this -> head;
-	int counter = 0;
-	while(current!=nullptr){
-		if(counter == index){
-			return current->pNext;
-		}
-		current = current->pNext;
-		counter++;
-	}
-}
-
-TE& List::operator[](const int index){	
-	Node *current = this -> head;
-	int counter = 0;
-	while(current!=nullptr){
-		if(counter == index){
-			return current->data;
-		}
-		current = current->pNext;
-		counter++;
-	}
-}
-
-
-int merge(List&, List&);
+int merge(List L1, List L2);
 
 #endif
